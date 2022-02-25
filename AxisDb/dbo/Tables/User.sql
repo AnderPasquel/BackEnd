33e78f9bb253b00147b0b846537fbe05a9c27774
Ander Pasquel <apasquel@samana.mx>
2022-02-25 09:42:00 -0600

    Agrego db

---
+++ AxisDb/dbo/Tables/User.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]       BIGINT        IDENTITY (1, 1) NOT NULL,
+    [Email]    VARCHAR (250) NOT NULL,
+    [UserName] VARCHAR (250) NOT NULL,
+    [Password] VARCHAR (250) NOT NULL,
+    [Status]   BIT           NOT NULL,
+    [Sex]      VARCHAR (4)   NOT NULL,
+    [Creation] DATETIME2 (7) NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
